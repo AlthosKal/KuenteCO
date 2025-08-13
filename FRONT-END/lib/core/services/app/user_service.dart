52093eb127de02c498c1cc13d817b2fc3d885ede
Exzidi <alejandromolinaacosta11@gmail.com>
2025-08-13 12:28:04 -0500

    FRONT guacala 4.3

--- FRONT-END/lib/core/services/app/user_service.dart
+++ FRONT-END/lib/core/services/app/user_service.dart
@@ -31,7 +31,7 @@ class UserService {
     });
 
     final response = await _apiClient.postApp('/auth/user/image/add', formData);
-    if (response.statusCode == 200) {
+    if (response.statusCode == 201) {
       return ImageDTO.fromJson(response.data);
     } else {
       throw Exception('Error al subir imagen: ${response.statusCode}');
