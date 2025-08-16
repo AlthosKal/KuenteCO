5b7ab6d13a004da9d881321299b09e6c66923779
Exzidi <alejandromolinaacosta11@gmail.com>
2025-08-15 19:29:14 -0500

    Categorias 1.1

--- FRONT-END/lib/dto/app/extra/description_category_extra.dart
+++ FRONT-END/lib/dto/app/extra/description_category_extra.dart
@@ -1,19 +1,16 @@
 import '../../../utils/enum/state_enum.dart';
 
 class DescriptionCategory {
-  final String name;
   final double assignedBudget;
   final State state;
 
   DescriptionCategory({
-    required this.name,
     required this.assignedBudget,
     required this.state,
   });
 
   factory DescriptionCategory.fromJson(Map<String, dynamic> json) {
     return DescriptionCategory(
-      name: json['name'],
       assignedBudget: (json['assignedBudget'] as num).toDouble(),
       state: State.values.firstWhere(
             (e) => e.name == json['state'],
@@ -24,7 +21,6 @@ class DescriptionCategory {
 
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
       'assignedBudget': assignedBudget,
       'state': state.name,
     };
