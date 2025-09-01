06e51ab592f55f19854aaf6e065c4e3493bff401
AlthosKal <agudelocastanoyeferson270@gmail.com>
2025-09-01 15:54:54 -0500

    Update transaction details

--- BACK-END/KuentecoApp/src/main/java/org/kuenteco/backend/mapper/logic/transaction/TransactionDetailMapper.java
+++ BACK-END/KuentecoApp/src/main/java/org/kuenteco/backend/mapper/logic/transaction/TransactionDetailMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransactionDetailMapper {
-    @Mapping(target = "timestamp", source = "transactionDate")
     @Mapping(target = "categoryId", source = "category.id")
     @Mapping(target = "budgetId", source = "budget.id")
     TransactionDetailDTO toDto(Transaction transaction);
